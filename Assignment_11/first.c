a946d9f0c7b5aed874b4011a6e55f5d5378ad9d5
Tarunsetha <tarunseth271@gmail.com>
2025-08-08 23:49:26 +0530

    init

---
+++ Assignment_11/first.c
@@ -0,0 +1,14 @@
+//Q1>Wap to print the pattern?
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=1;i<=4;i++){
+        for(j=1;j<=7;j++){
+            if(j>=5-i&&j<=3+i)
+            printf("*");
+            else
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
---
+++ Assignment_11/second.c
@@ -0,0 +1,14 @@
+//Q2>Wap to print the pattern?
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=1;i<=4;i++){
+        for(j=1;j<=7;j++){
+            if(j>=i&&j<=8-i)
+            printf("*");
+            else
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
