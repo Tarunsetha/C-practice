d1a45edb3863d7b9dc6d1cf9ae3c95ce0095f59b
Tarunsetha <tarunseth271@gmail.com>
2025-08-28 17:58:39 +0530

    init

--- Assignment_14/nineth.c
+++ Assignment_14/nineth.c
@@ -1,22 +1,47 @@
 //Q9>WAF to print all the Armstrong numbers between to given numbers?(TSRN)
-#include<stdio.h>
-void armstrong(int ,int);
-int main(){
-    int d,e;
-    printf("enter the number ");
-    scanf("%d%d",&d,&e);
-    armstrong(d,e);
-}
-void armstrong(int n,int m){
-    int a,sum=0,b=1;
-    for(int i=n;i<=m;i++){
-        a=n%10;
-        n=n/10;
-        for(j=1;j<=i;j++){
-            b=b*a;
-             sum=sum+b;
-        }
-        
+#include <stdio.h>
 
+void armstrong_between(int, int);
+
+int main() {
+    int start, end;
+    printf("Enter two numbers (range): ");
+    scanf("%d %d", &start, &end);
+
+    armstrong_between(start, end);
+
+    return 0;
+}
+
+void armstrong_between(int a, int b) {
+    int num, temp, digit, sum, n, i;
+
+    printf("Armstrong numbers between %d and %d are:\n", a, b);
+
+    for (num = a; num <= b; num++) {
+        sum = 0;
+        temp = num;
+
+        n = 0;
+        int t = num;
+        while (t > 0) {   
+            n++;
+            t =t/ 10;
+        }
+
+        t = num;
+        while (t > 0) {   
+            digit = t % 10;
+            int power = 1;
+            for (i = 0; i < n; i++) {
+                power =power*digit;
+            }
+            sum =sum+ power;
+            t =t/10;
+        }
+
+        if (sum == num) {
+            printf("%d\n", num);
+        }
     }
 }
