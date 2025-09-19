791aa9af5e7304cda05092c425b774e233701fc5
Tarunsetha <tarunseth271@gmail.com>
2025-09-19 21:09:16 +0530

    init

--- Assignment_18/seveth.c
+++ Assignment_18/seveth.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int total_number_of_duplicate_elements(int b[],int size);
 int main(){
-    int arr[10],i,k,n;
+    int arr[10],i,result,n;
     printf("Enter size of array: ");
     scanf("%d", &n);
 
@@ -10,20 +10,20 @@ int main(){
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);   
     }
-    k=total_number_of_duplicate_elements( arr,n);
-  printf("Total number of elements that occur exactly %d times",k);
+    result =total_number_of_duplicate_elements( arr,n);
+  printf("Total number of elements that occur exactly %d times",result);
 
 }
-int total_number_of_duplicate_elements(int b[],int size){
+int  total_number_of_duplicate_elements(int b[],int size){
     int i,j,k;
     for(i=0,k=0;i<size;i++){
         for(j=i+1;j<size;j++){
             if(b[i]==b[j]){
-             
+             k++;
             }
         }
-        return k;
     }
+  return k;
     
 }
 
