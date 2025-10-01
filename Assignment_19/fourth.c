321f8dd37ceea682520fc41e35e3be4bd2f9a2c9
Tarunsetha <tarunseth271@gmail.com>
2025-10-01 21:40:01 +0530

    init

---
+++ Assignment_19/fourth.c
@@ -0,0 +1,1 @@
+//Q4>Four players are playing a tournament of Chess with round robin method (each player will play with every other player). Each win has 2 points, draw has 1 point and loose has 0 points. Declare a score_board two dimensional array to store the scores of the players agains each player?
